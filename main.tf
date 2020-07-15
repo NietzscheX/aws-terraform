3c1cead96f8438efd2eced56213249fcd0ba6a84
libai <libai@libais-iMac.local>
2020-07-15 14:59:44 +0800

    add security group for ecs2

--- main.tf
+++ main.tf
@@ -5,8 +5,34 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
   tags = {
     Name = "terraform-example"
   }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello world" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
+
 }
+
+##for security group
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress{
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+## tell ec2 instance to use the security group
+## use terraform expressions
+## so i need add vpc_security_group_ids to  aws_instance example
 
